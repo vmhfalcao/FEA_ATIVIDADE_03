84d1f980acf2094ed3875656c1104f8fcabdee10
vmhfalcao <50721438+vmhfalcao@users.noreply.github.com>
2019-07-04 22:35:48 -0300

    primeiro commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './img/favicon.ico'
+import './css/index.css'
+
+ReactDOM.render(
+ <h1>Desenvolvimento Front-end Avançado</h1>,
+ document.querySelector("#main")
+)
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+module.exports = {
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|ico|png|gif|svg)$/i,
+                loader: 'file-loader?name=img/[name].[ext]'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'src/index.html')
+        }),
+        new ExtractTextPlugin('style.css'),
+        new UglifyJSPlugin() 
+    ],
+
+    devServer: {
+        publicPath: "/",
+        contentBase: "./dist"
+    }
+};
